b95196aef59bbae417a91839e162fa3993cd5ac4
winst13 <winstonwang12@gmail.com>
2018-02-26 19:51:58 -0800

    test2

--- code/main.py
+++ code/main.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# hahaha extra comments
+# hahaha extra comments take 2
 
 """This file contains the entrypoint to the rest of the code"""
 
